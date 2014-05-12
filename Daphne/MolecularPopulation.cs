0a2859230708b6495a1baf15df3d0ca19c5f6ba2
gkepler <gkepler>
2014-05-12 22:44:32 +0000

    Corrected error in UpdateBoundary() in MolecularPopulation.cs (TFS Integration from 'hippocrates.bumc.bu.edu (VC)' Id: 1435;CreationDate: 2013-02-12 21:41:17 UTC;)
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-main;C2435

--- Daphne/MolecularPopulation.cs
+++ Daphne/MolecularPopulation.cs
@@ -208,20 +208,18 @@ namespace Daphne
                     // BoundedRectangles -> BoundedRectangularPrism
                     //                      arraySize > 1, one-to-one correspondence
                     // 
-                    for (int k = 0; k < BoundaryConcs[m].array.Length; k++)
-                    {
-                        // NOTE: This corresponds to the case where there is a one-to-one
-                        // correspondance between the array points of the embedding and embedded manifolds
+                    // NOTE: This corresponds to the case where there is a one-to-one
+                    // correspondance between the array points of the embedding and embedded manifolds
 
-                        // Feed embedded manifold array index k to WhereIs and return a double[] point in the embedding manifold
-                        // request interpolation if needed
+                    // Feed embedded manifold array index k to WhereIs and return a double[] point in the embedding manifold
+                    // request interpolation if needed                   
                     if (Man.Boundaries[m].NeedsInterpolation() == true)
                     {
                         for (int k = 0; k < BoundaryConcs[m].array.Length; k++)
                         {
                             BoundaryConcs[m].array[k] = Concentration(Man.Boundaries[m].WhereIs(k));
                             BoundaryGlobalGrad[m].vector[k] = GlobalGradient(Man.Boundaries[m].WhereIs(k));
-                         }
+                        }
                     }
                     else
                     {
@@ -229,8 +227,6 @@ namespace Daphne
                         {
                             BoundaryConcs[m].array[k] = Concentration(Man.Boundaries[m].WhereIsIndex(k));
                             BoundaryGlobalGrad[m].vector[k] = GlobalGradient(Man.Boundaries[m].WhereIsIndex(k));
-                        }
-                    }
                         }
                     }
                 }
