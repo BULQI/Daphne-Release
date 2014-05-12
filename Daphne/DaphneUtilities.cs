46ad4e1672faff0df2f28d85954b51006589a01f
gkepler <gkepler>
2014-05-12 22:49:15 +0000

    Utilities for random number generator and distributions. (TFS Integration from 'hippocrates.bumc.bu.edu (VC)' Id: 2382;CreationDate: 2014-01-16 20:39:17 UTC;)
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-main;C2724

---
+++ Daphne/DaphneUtilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.RandomSources;
+
+namespace Daphne
+{
+    /// <summary>
+    /// Distributions and random number sources.
+    /// This code was copied from Plaza Sur.
+    /// </summary>
+    class Rand
+    {
+        public static MersenneTwisterRandomSource MersenneTwister;
+        public static NormalDistribution NormalDist;
+        public static ContinuousUniformDistribution UniformDist;
+        //public static SystemRandomSource SystemRandom;
+
+        static Rand()
+        {
+            MersenneTwister = new MersenneTwisterRandomSource();
+            NormalDist = new NormalDistribution(MersenneTwister);
+            NormalDist.SetDistributionParameters(0.0, 1.0);
+            UniformDist = new ContinuousUniformDistribution(MersenneTwister);
+            UniformDist.SetDistributionParameters(0.0, 1.0);
+        }
+
+        public static void ReseedAll(int seed)
+        {
+            MersenneTwister = new MersenneTwisterRandomSource(seed);
+            NormalDist = new NormalDistribution(MersenneTwister);
+            NormalDist.SetDistributionParameters(0.0, 1.0);
+            UniformDist = new ContinuousUniformDistribution(MersenneTwister);
+            UniformDist.SetDistributionParameters(0.0, 1.0);
+            //SystemRandom = new SystemRandomSource(seed);
+        }
+
+        public static void ReseedNormalDist(int seed)
+        {
+            if (seed > 0)
+            {
+                MersenneTwister = new MersenneTwisterRandomSource(seed);
+                NormalDist = new NormalDistribution(MersenneTwister);
+                NormalDist.SetDistributionParameters(0.0, 1.0);
+            }
+        }
+
+        public static void ReseedUniformDist(int seed)
+        {
+            if (seed > 0)
+            {
+                MersenneTwister = new MersenneTwisterRandomSource(seed);
+                UniformDist = new ContinuousUniformDistribution(MersenneTwister);
+                UniformDist.SetDistributionParameters(0.0, 1.0);
+            }
+        }
+    }
+
+}
