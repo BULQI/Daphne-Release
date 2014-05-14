49c12dfda479cd5c4e802393c778edf092543358
Gupta, Sanjeev <skgupta@bu.edu>
2014-05-14 19:17:15 +0000

    Added an About dialog box with version info.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-skg;C2946

---
+++ DaphneGui/About.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.IO;
+
+namespace DaphneGui
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+
+            Assembly ass = Assembly.GetExecutingAssembly();
+            string exename = ass.CodeBase;
+            exename = new Uri(exename).LocalPath;
+            txtVersion.Text = "Version: " + File.GetCreationTime(exename).ToLongDateString() + ", " + File.GetCreationTime(exename).ToLongTimeString();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
