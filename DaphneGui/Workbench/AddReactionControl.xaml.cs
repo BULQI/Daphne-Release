0cce97d3f273ace3336671416478f8fee1432b9f
Hua, Axin <ahua@bu.edu>
2014-06-19 15:00:33 +0000

    rollback 4209
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-main;C4210

--- DaphneGui/Workbench/AddReactionControl.xaml.cs
+++ DaphneGui/Workbench/AddReactionControl.xaml.cs
@@ -513,11 +513,11 @@ namespace DaphneGui
             CompositeCollection coll = new CompositeCollection();
 
             CollectionContainer cc = new CollectionContainer();
-            cc.Collection = MainWindow.SOP != null ? MainWindow.SOP.Protocol.entity_repository.molecules : null;
+            cc.Collection = MainWindow.SOP.Protocol.entity_repository.molecules;
             coll.Add(cc);
 
             cc = new CollectionContainer();
-            cc.Collection = MainWindow.SOP != null ? MainWindow.SOP.Protocol.entity_repository.genes : null;
+            cc.Collection = MainWindow.SOP.Protocol.entity_repository.genes;
             coll.Add(cc);
             lbMol2.SetValue(ListBox.ItemsSourceProperty, coll);
             lbMol2.SetValue(ListBox.DisplayMemberPathProperty, "Name");
