c9de04625a739f8494fd84feced5c9fe744dd5f2
Kepler, Grace <gmkepler@bu.edu>
2014-10-22 12:36:10 +0000

    Removed unneeded reference to  DaphneUserControlLib.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-gui-refactor;C4801

--- DaphneGui/CellPropertiesControl.xaml.cs
+++ DaphneGui/CellPropertiesControl.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Daphne;
-using DaphneUserControlLib;
 using System.Globalization;
 
 namespace DaphneGui
