050e45d89ea264048040fde81f1e25d080af62ad
Kepler, Grace <gmkepler@bu.edu>
2014-10-24 13:12:48 +0000

    Derived workbench classes.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-gui-refactor;C4808

---
+++ DaphneGui/ToolWinVatRC.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Daphne;
+
+namespace DaphneGui
+{
+    /// <summary>
+    /// Interaction logic for ToolWinVatRC.xaml
+    /// </summary>
+    public partial class ToolWinVatRC : ToolWinBase
+    {
+        public ToolWinVatRC()  : base()
+        {
+            InitializeComponent();
+            TitleText = "Vat Reaction Complex workbench";
+        }
+
+        public override void RegionFocusToGUISection(RegionWidget rw, bool transferMatrix)
+        {
+            throw new Exception("VatReactionComplex does not implement RegionFocusToGUISection method.");
+        }
+
+        public override void SelectMolpopInGUI(int index)
+        {
+            throw new Exception("VatReactionComplex does not implement SelectMolpopInGUI method.");
+        }
+
+
+    }
+}
