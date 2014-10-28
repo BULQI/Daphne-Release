dad6b8d9cd4aae28453639fa2670127a503e07c0
Kepler, Grace <gmkepler@bu.edu>
2014-10-28 18:07:12 +0000

    Implementation of Reports tab in TissueSimulation. Preliminary relocation of some SimConfigToolWindow classes to base and derived classes.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-gui-refactor-grace;C4860

--- DaphneGui/ToolWinBase.cs
+++ DaphneGui/ToolWinBase.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using ActiproSoftware.Windows.Controls.Docking;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
 
 using Daphne;
 
@@ -35,14 +37,230 @@ namespace DaphneGui
 
 
         /// <summary>
+        /// Check for a molecule in the specified compartment of the cell.
+        /// </summary>
+        /// <param name="molguid"></param>
+        /// <param name="isMembrane"></param>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        protected virtual bool CellHasMolecule(string molguid, bool isMembrane, ConfigCell cell)
+        {
+            if (isMembrane == true)
+            {
+                if (CompartmentHasMolecule(molguid, cell.membrane))
+                {
+                    return true;
+                }
+            }
+            else if (CompartmentHasMolecule(molguid, cell.membrane))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Return true if the molecule is located in the specified compartment (cytosol or membrane).
+        /// Modified and reorganized from previous code. 
+        /// Has not been evaluated yet.
+        /// </summary>
+        /// <param name="molguid"></param>
+        /// <param name="isMembrane"></param>
+        /// <returns></returns>
+        protected virtual bool CellPopsHaveMolecule(string molguid, bool isMembrane)
+        {
+            foreach (CellPopulation cell_pop in ((TissueScenario)Protocol.scenario).cellpopulations)
+            {
+                if (CellHasMolecule(molguid, isMembrane, cell_pop.Cell) == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Check to see if a compartment contains a molecular population of type molecule.
+        /// gmk - Modified and reorganized from previous code. Needs evaluation.
+        /// </summary>
+        /// <param name="molguid"></param>
+        /// <param name="compartment"></param>
+        /// <returns></returns>
+        protected bool CompartmentHasMolecule(string molguid, ConfigCompartment compartment)
+        {
+            foreach (ConfigMolecularPopulation molpop in compartment.molpops)
+            {
+                if (molpop.molecule.entity_guid == molguid)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
         /// Functionality to refresh elements when the selected tab changes
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         protected virtual void ConfigTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            throw new Exception("This method must be implemented in a derived class.");
         }
 
+        /// <summary>
+        /// Filter (return true) for reaction that has necessary molecules in the environment comp and one or more cell membrane.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void ecmAvailableReactionsListView_Filter(object sender, FilterEventArgs e)
+        {
+            ConfigReaction cr = e.Item as ConfigReaction;
+            bool bOK = false;
+
+            foreach (string molguid in cr.reactants_molecule_guid_ref)
+            {
+                //if (EcmHasMolecule(molguid) || CellPopsHaveMoleculeInMemb(molguid))
+                if (CompartmentHasMolecule(molguid, Protocol.scenario.environment.comp) || CellPopsHaveMolecule(molguid, true))
+                {
+                    bOK = true;
+                }
+                else
+                {
+                    bOK = false;
+                    break;
+                }
+            }
+            if (bOK)
+            {
+                foreach (string molguid in cr.products_molecule_guid_ref)
+                {
+                    //if (EcmHasMolecule(molguid) || CellPopsHaveMoleculeInMemb(molguid))
+                    if (CompartmentHasMolecule(molguid, Protocol.scenario.environment.comp) || CellPopsHaveMolecule(molguid, true))
+                    {
+                        bOK = true;
+                    }
+                    else
+                    {
+                        bOK = false;
+                        break;
+                    }
+                }
+            }
+            if (bOK)
+            {
+                foreach (string molguid in cr.modifiers_molecule_guid_ref)
+                {
+                    //if (EcmHasMolecule(molguid) || CellPopsHaveMoleculeInMemb(molguid))
+                    if (CompartmentHasMolecule(molguid, Protocol.scenario.environment.comp) || CellPopsHaveMolecule(molguid, true))
+                    {
+                        bOK = true;
+                    }
+                    else
+                    {
+                        bOK = false;
+                        break;
+                    }
+                }
+            }
+
+            //Finally, if the ecm already contains this reaction, exclude it from the available reactions list
+            if (MainWindow.SOP.Protocol.scenario.environment.comp.reactions_dict.ContainsKey(cr.entity_guid) == true)
+            {
+                bOK = false;
+            }
+
+            e.Accepted = bOK;
+        }
+
+        /// <summary>
+        /// Filter for bulk molecules. 
+        /// gmk - Rename? Should be usable by all workbenches.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void EcsMoleculesListView_Filter(object sender, FilterEventArgs e)
+        {
+            ConfigMolecule mol = e.Item as ConfigMolecule;
+            if (mol == null || mol.molecule_location != MoleculeLocation.Bulk)
+            {
+                e.Accepted = false;
+                return;
+            }
+            e.Accepted = true;
+            return;
+        }
+
+        /// <summary>
+        /// gmk - Do we need this? Why only for bulkMoleculesListView_Filter? 
+        /// Okay for all workbenches?
+        /// </summary>
+        public class FilterFactory
+        {
+            private object Context { get; set; }
+
+            public static void bulkMoleculesListView_Filter(object sender, FilterEventArgs e)
+            {
+                ConfigMolecule mol = e.Item as ConfigMolecule;
+                if (mol != null)
+                {
+                    // Filter out mol if membrane bound 
+                    if (mol.molecule_location == MoleculeLocation.Bulk)
+                    {
+                        e.Accepted = true;
+                    }
+                    else
+                    {
+                        e.Accepted = false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converter to go between molecule GUID references in MolPops
+        /// and molecule names kept in the repository of molecules.
+        /// </summary>
+        [ValueConversion(typeof(string), typeof(string))]
+        public class MolGUIDtoMolNameConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                string guid = value as string;
+                string mol_name = "";
+
+                if (parameter == null || guid == "")
+                    return mol_name;
+
+                System.Windows.Data.CollectionViewSource cvs = parameter as System.Windows.Data.CollectionViewSource;
+                ObservableCollection<ConfigMolecule> mol_list = cvs.Source as ObservableCollection<ConfigMolecule>;
+                if (mol_list != null)
+                {
+                    foreach (ConfigMolecule mol in mol_list)
+                    {
+                        if (mol.entity_guid == guid)
+                        {
+                            mol_name = mol.Name;
+                            break;
+                        }
+                    }
+                }
+                return mol_name;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                // TODO: Should probably put something real here, but right now it never gets called,
+                // so I'm not sure what the value and parameter objects would be...
+                return "y";
+            }
+        }
+
+        /// <summary>
+        /// Moved from SimConfigToolWindow.xaml.cs but not evaluated.
+        /// </summary>
+        /// <param name="rw"></param>
+        /// <param name="transferMatrix"></param>
         public virtual void RegionFocusToGUISection(RegionWidget rw, bool transferMatrix)
         {
             // identify the widget's key
@@ -101,6 +319,25 @@ namespace DaphneGui
             }
         }
 
+        /// <summary>
+        /// Moved from SimConfigToolWindow.xaml.cs but not evaluated.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void ReportsTabItem_Loaded(object sender, RoutedEventArgs e)
+        {
+            // gmk - uncomment and fix
+            //lbRptCellPops.SelectedIndex = 0;
+            //ICollectionView icv = CollectionViewSource.GetDefaultView(lvAvailableReacs.ItemsSource);
+            //if (icv != null)
+            //{
+            //    icv.Refresh();
+            //}
+        }
+
+        /// <summary>
+        /// Moved from SimConfigToolWindow.xaml.cs but not evaluated.
+        /// </summary>
         protected virtual void Save_Selected_Tab()
         {
             // Called by MainWindow.resetButton_Click
@@ -121,16 +358,14 @@ namespace DaphneGui
             //}
         }
 
-        public void SelectGaussSpecInGUI(int index, string guid)
+        /// <summary>
+        /// Moved from SimConfigToolWindow.xaml.cs but not evaluated.
+        /// May need to move method body into override version in TissueSimulation.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="guid"></param>
+        public virtual void SelectGaussSpecInGUI(int index, string guid)
         {
-            // this window seems to implement the tissue scenario gui; throw an exception for now to enforce that;
-            // Sanjeev, you probably need to have a hierachy of tool windows where each implements the gui for one case,
-            // but I don't know for sure; we can discuss
-            if (MainWindow.SOP.Protocol.CheckScenarioType(Protocol.ScenarioType.TISSUE_SCENARIO) == false)
-            {
-                throw new InvalidCastException();
-            }
-
             TissueScenario scenario = (TissueScenario)MainWindow.SOP.Protocol.scenario;
             bool isBoxInCell = false;
 
@@ -154,11 +389,11 @@ namespace DaphneGui
             // gmk - uncomment
             if (isBoxInCell == true)
             {
-            //    MW.ConfigTabControl.SelectedItem = tabCellPop;
-            //}
-            //else
-            //{
-            //    MW.ConfigTabControl.SelectedItem = tabECM;
+                //    MW.ConfigTabControl.SelectedItem = tabCellPop;
+                //}
+                //else
+                //{
+                //    MW.ConfigTabControl.SelectedItem = tabECM;
             }
         }
 
@@ -168,16 +403,10 @@ namespace DaphneGui
             //lbEcsMolPops.SelectedIndex = index;
         }
 
-        protected virtual void ReportsTabItem_Loaded(object sender, RoutedEventArgs e)
-        {
-            // gmk - uncomment and fix
-            //lbRptCellPops.SelectedIndex = 0;
-            //ICollectionView icv = CollectionViewSource.GetDefaultView(lvAvailableReacs.ItemsSource);
-            //if (icv != null)
-            //{
-            //    icv.Refresh();
-            //}
-        }
+
+
+
+
 
 
     }
--- DaphneGui/ToolWinVatRC.xaml.cs
+++ DaphneGui/ToolWinVatRC.xaml.cs
@@ -25,21 +25,60 @@ namespace DaphneGui
             TitleText = "Vat Reaction Complex";
             ToroidalVisibility = Visibility.Hidden;
             SimRepetitionVisibility = Visibility.Hidden;
-            InitializeComponent();
 
+            InitializeComponent();
             DataContext = this;
         }
 
         public override void Apply()
         {
             TabItem selectedTab = toolWinVatRC.ConfigTabControl.SelectedItem as TabItem;
+
+            int reportVatMolSelectedIndex = -1;
+            if (selectedTab == tabReports)
+            {
+                //reportVatMolSelectedIndex = ((DataGrid)Vat_MoleculeSettings.Content).SelectedIndex;
+            }
+
+
             MW.Apply();
+
             toolWinVatRC.ConfigTabControl.SelectedItem = selectedTab;
+            if (selectedTab == toolWinVatRC.tabReactionComplexes)
+            {
+                // gmk - add code to reset the selected RC
+            }
+            else if (selectedTab == toolWinVatRC.tabReports)
+            {
+                //((DataGrid)Vat_MoleculeSettings.Content).SelectedIndex = reportVatMolSelectedIndex = -1;
+            }
+
         }
+
+        protected override bool CellHasMolecule(string molguid, bool isMembrane, ConfigCell cell)
+        {
+            throw new Exception("VatReactionComplex does not implement CellHasMolecule method.");
+        }
+
+        protected override bool CellPopsHaveMolecule(string molguid, bool isMembrane)
+        {
+            throw new Exception("VatReactionComplex does not implement CellPopsHaveMolecule method.");
+        }
+
+        protected virtual void ecmAvailableReactionsListView_Filter(object sender, FilterEventArgs e)
+        {
+            throw new Exception("VatReactionComplex does not implement ecmAvailableReactionsListView_Filter method.");
+        }
+
 
         public override void RegionFocusToGUISection(RegionWidget rw, bool transferMatrix)
         {
             throw new Exception("VatReactionComplex does not implement RegionFocusToGUISection method.");
+        }
+
+        public virtual void SelectGaussSpecInGUI(int index, string guid)
+        {
+            throw new Exception("VatReactionComplex does not implement SelectGaussSpecInGUI method.");
         }
 
         public override void SelectMolpopInGUI(int index)
