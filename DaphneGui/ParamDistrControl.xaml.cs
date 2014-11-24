867e52be4e3306fcbcf1aecc89d62634c54eebeb
Kepler, Grace <gmkepler@bu.edu>
2014-11-24 00:40:40 +0000

    Preliminary implementaton of categorical distribution in GUI and fixes to other probability distributions.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-grace;C5135

--- DaphneGui/ParamDistrControl.xaml.cs
+++ DaphneGui/ParamDistrControl.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DaphneUserControlLib
+using Daphne;
+using DaphneUserControlLib;
+
+namespace DaphneGui
 {
     /// <summary>
     /// Interaction logic for UserControl1.xaml
@@ -33,16 +36,79 @@ namespace DaphneUserControlLib
                 return;
             }
 
-            //DistributedParameter distrParam = (DistributedParameter)ParamDistrType;
+            DistributedParameter distr_parameter = DataContext as DistributedParameter;
+            if (distr_parameter == null)
+            {
+                return;
+            }
 
+            ParameterDistributionType selected_distr_type = (ParameterDistributionType)cbParamDistr.SelectedItem;
+            if (selected_distr_type == distr_parameter.ParamDistr.DistributionType)
+            {
+                return;
+            }
 
+            switch (selected_distr_type)
+            {
+                case ParameterDistributionType.CONSTANT:
+                    ConstantParameterDistribution new_constant_distr = new ConstantParameterDistribution();
+                    distr_parameter.ParamDistr = new_constant_distr;
+                    break;
 
+                case ParameterDistributionType.CATEGORICAL:
+                    CategoricalParameterDistribution new_categorical_distr = new CategoricalParameterDistribution();
+                    distr_parameter.ParamDistr = new_categorical_distr;
+                    break;
 
+                case ParameterDistributionType.GAMMA:
+                    GammaParameterDistribution new_gamma_distr = new GammaParameterDistribution();
+                    distr_parameter.ParamDistr = new_gamma_distr;
+                    break;
 
+                case ParameterDistributionType.POISSON:
+                    PoissonParameterDistribution new_poisson_distr = new PoissonParameterDistribution();
+                    distr_parameter.ParamDistr = new_poisson_distr;
+                    break;
 
+                case ParameterDistributionType.UNIFORM:
+                    UniformParameterDistribution new_uniform_distr = new UniformParameterDistribution();
+                    distr_parameter.ParamDistr = new_uniform_distr;
+                    break;
 
+                default:
+                    break;
 
+            }
+        }
+
+        private void dgProbMass_KeyDown(object sender, KeyEventArgs e)
+        {
 
         }
+
+        private void dgProbMass_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+
+        private void dgProbMass_Check(object sender, DataTransferEventArgs e)
+        {
+        }
+
+        private void menuProbMassPaste_Click(object sender, DataTransferEventArgs e)
+        {
+        }
+
+        private void dgProbMass_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuProbMassPaste_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
+
 }
