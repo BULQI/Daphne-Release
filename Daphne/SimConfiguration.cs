76585dd610671715f1336ff8346b2cc0e1c0e006
Gupta, Sanjeev <skgupta@bu.edu>
2015-01-17 00:57:28 +0000

    Removed getReacLeftSide and getReacRightSide from Protocol class. Added a "DeleteGene" method to ConfigTransitionScheme class.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-main;C5429

--- Daphne/SimConfiguration.cs
+++ Daphne/SimConfiguration.cs
@@ -1922,7 +1922,14 @@ namespace Daphne
         }
 
         
-        // given a total reaction string, find the ConfigCell object
+        
+        /// <summary>
+        /// Given a total reaction string, find it in the reactions list.
+        /// Return true if found, false otherwise.
+        /// </summary>
+        /// <param name="total"></param>
+        /// <param name="Reacs"></param>
+        /// <returns></returns>
         public bool findReactionByTotalString(string total, ObservableCollection<ConfigReaction> Reacs)
         {
             //Get left and right side molecules of new reaction
@@ -1958,45 +1965,6 @@ namespace Daphne
             return false;
         }
         
-        /// <summary>
-        /// This method takes the ConfigReaction's TotalReactionString and returns a sorted 
-        /// list of molecule strings on the left side, i.e. the reactants.
-        /// </summary>
-        /// <param name="total"></param>
-        /// <returns></returns>
-        private List<string> getReacLeftSide(string total)
-        {
-            int len = total.Length;
-            int index = total.IndexOf("->");
-            string left = total.Substring(0, index);
-            left = left.Replace(" ", "");
-            char[] separator = { '+' };
-            string[] reactants = left.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-            List<string> listLeft = new List<string>(reactants);
-            listLeft.Sort();
-            return listLeft;
-        }
-
-        /// <summary>
-        /// This method takes the ConfigReaction's TotalReactionString and returns a sorted 
-        /// list of molecule strings on the right side, i.e. the products.
-        /// </summary>
-        /// <param name="total"></param>
-        /// <returns></returns>
-        private List<string> getReacRightSide(string total)
-        {
-            int len = total.Length;
-            int index = total.IndexOf("->");
-            string right = total.Substring(index + 2);
-            right = right.Replace(" ", "");
-            char[] separator = { '+' };
-            string[] products = right.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-            List<string> listRight = new List<string>(products);
-            listRight.Sort();
-            return listRight;
-        }
-
-
         /// <summary>
         /// Select transcription reactions in the compartment.
         /// </summary>
@@ -4449,6 +4417,17 @@ namespace Daphne
             foreach (ConfigActivationRow row in activationRows)
             {
                 row.activations.Add(1.0);
+            }
+        }
+
+        public void DeleteGene(string gguid)
+        {
+            int index = genes.IndexOf(gguid);
+            genes.Remove(gguid);
+
+            foreach (var v in activationRows)
+            {
+                v.activations.RemoveAt(index);
             }
         }
 
