814383712bfe51c3c4aa72d0b089fa60d195b24b
Gupta, Sanjeev <skgupta@bu.edu>
2015-03-05 19:23:12 +0000

    Bug 2426 - Added code to make available to ECM any reaction except those with genes. If any molecules or genes are missing from the cell or ECM, these get added. Still to do Cell Membrane and Cytosol.
    git-tfs-id: [http://tfs.bumc.bu.edu:8080/tfs/BU-MICRO-MSI]$/Daphne/Daphne-skg;C5624

--- DaphneGui/ECMMolpopControl.xaml.cs
+++ DaphneGui/ECMMolpopControl.xaml.cs
@@ -83,18 +83,125 @@ namespace DaphneGui
             MolDetailsExpander.IsExpanded = true;
         }
 
+        private void ProcessBoundaryMoleculeAvailReac(ConfigMolecule mol)
+        {
+            bool cellHasMolecule = false;
+
+            // Check to see if any of the cells have the boundary molecule in their membrane.
+            // If this boundary molecule exists on at least one cell, then we are done.
+            foreach (CellPopulation cellpop in ((TissueScenario)MainWindow.SOP.Protocol.scenario).cellpopulations)
+            {
+                if (cellpop.Cell.membrane.HasMolecule(mol))
+                {
+                    cellHasMolecule = true;
+                    break;
+                }
+            }
+
+            // Otherwise, see if the user wants to add this boundary molecule to any of the cells.
+            if (cellHasMolecule == false)
+            {
+                string message = ("One or more reactions depend on molecule " + mol.Name + ", which is not currently in the simulation. ");
+                message = message + "You will be prompted to add this molecule to the cell membrane of one, or more, of the cell populations.";
+                MessageBox.Show(message);
+
+                foreach (CellPopulation cellpop in ((TissueScenario)MainWindow.SOP.Protocol.scenario).cellpopulations)
+                {
+                    message = "Add molecule " + mol.Name + " to cell population " + cellpop.cellpopulation_name + "?";
+                    MessageBoxResult result = MessageBox.Show(message, "Question", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        cellpop.Cell.membrane.AddMolPop(mol, false);
+                        cellHasMolecule = true;
+                    }
+                }
+
+                // Finally, if the user did not add the missing molecule to any of the cells, 
+                // issue a warning that some reactions won't be included in the simulation.
+                if (cellHasMolecule == false)
+                {
+                    message = "Molecule " + mol.Name + " was not added to any of the cells. ";
+                    message = message + "Reactions that require this molecule will not be included in the simulation.";
+                    MessageBox.Show(message, "Warning");
+                }
+            }
+        }
+
         private void AddEcmReacButton_Click(object sender, RoutedEventArgs e)
         {
             bool needRefresh = false;
 
+            ObservableCollection<ConfigReaction> reactionsToAdd = new ObservableCollection<ConfigReaction>();
             foreach (var item in lvAvailableReacs.SelectedItems)
+            {
+                reactionsToAdd.Add(item as ConfigReaction);
+            }
+
+            foreach (var item in reactionsToAdd)
             {
                 ConfigReaction reac = (ConfigReaction)item;
 
                 if (MainWindow.SOP.Protocol.scenario.environment.comp.reactions_dict.ContainsKey(reac.entity_guid) == false)
                 {
+                    string message = "If the ECM does not currently contain any of the molecules necessary for these reactions, then they will be added. ";
+                    message = message + "Any duplicate reactions currently in the ECM will be removed. Continue?";
+                    MessageBoxResult result = MessageBox.Show(message, "Warning", MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.No)
+                    {
+                        return;
+                    }
+
                     MainWindow.SOP.Protocol.scenario.environment.comp.Reactions.Add(reac.Clone(true));
                     needRefresh = true;
+
+                    //If any molecules from new reaction don't exist in the ecm, add them if bulk
+                    foreach (string molguid in reac.reactants_molecule_guid_ref)
+                    {
+                        ConfigMolecule mol = MainWindow.SOP.Protocol.entity_repository.molecules_dict[molguid];
+                        //Bulk
+                        if (mol.molecule_location == MoleculeLocation.Bulk)
+                        {
+                            if (MainWindow.SOP.Protocol.scenario.environment.comp.HasMolecule(molguid) == false)
+                            {
+                                MainWindow.SOP.Protocol.scenario.environment.comp.AddMolPop(mol, false);
+                            }
+                        }
+                        else  //If Boundary, then see if any of the cells have this molecule.
+                        {
+                            ProcessBoundaryMoleculeAvailReac(mol);
+                        }
+                    }
+                    foreach (string molguid in reac.products_molecule_guid_ref)
+                    {
+                        ConfigMolecule mol = MainWindow.SOP.Protocol.entity_repository.molecules_dict[molguid];
+                        if (mol.molecule_location == MoleculeLocation.Bulk)
+                        {
+                            if (MainWindow.SOP.Protocol.scenario.environment.comp.HasMolecule(molguid) == false)
+                            {
+                                MainWindow.SOP.Protocol.scenario.environment.comp.AddMolPop(mol, false);
+                            }
+                        }
+                        else  //If Boundary, then see if any of the cells have this molecule.
+                        {
+                            ProcessBoundaryMoleculeAvailReac(mol);
+                        }
+                    }
+                    foreach (string molguid in reac.modifiers_molecule_guid_ref)
+                    {
+                        ConfigMolecule mol = MainWindow.SOP.Protocol.entity_repository.molecules_dict[molguid];
+                        if (mol.molecule_location == MoleculeLocation.Bulk)
+                        {
+                            if (MainWindow.SOP.Protocol.scenario.environment.comp.HasMolecule(molguid) == false)
+                            {
+                                MainWindow.SOP.Protocol.scenario.environment.comp.AddMolPop(mol, false);
+                            }
+                        }
+                        else  //If Boundary, then see if any of the cells have this molecule.
+                        {
+                            ProcessBoundaryMoleculeAvailReac(mol);
+                        }
+                    }
                 }
             }
 
@@ -338,33 +445,9 @@ namespace DaphneGui
         private bool reactionIsAvailable(ConfigReaction cr)
         {
             bool bOK = true;
-            foreach (string molguid in cr.reactants_molecule_guid_ref)
-            {
-                //if (MainWindow.ToolWin.CompartmentHasMolecule(molguid, MainWindow.ToolWin.Protocol.scenario.environment.comp) == false
-                if (MainWindow.ToolWin.Protocol.scenario.environment.comp.HasMolecule(molguid) == false
-                        && MainWindow.ToolWin.CellPopsHaveMolecule(molguid, true) == false )
-                {
-                    return false;
-                }
-            }
 
-            foreach (string molguid in cr.products_molecule_guid_ref)
-            {
-                if (MainWindow.ToolWin.Protocol.scenario.environment.comp.HasMolecule(molguid) == false
-                            && MainWindow.ToolWin.CellPopsHaveMolecule(molguid, true) == false)
-                {
-                    return false;
-                }
-            }
-
-            foreach (string molguid in cr.modifiers_molecule_guid_ref)
-            {
-                if (MainWindow.ToolWin.Protocol.scenario.environment.comp.HasMolecule(molguid) == false
-                            && MainWindow.ToolWin.CellPopsHaveMolecule(molguid, true) == false)
-                {
-                    return false;
-                }
-            }
+            if (cr.HasGene(MainWindow.ToolWin.Protocol.entity_repository))
+                return false;
 
             //Finally, if the ecm already contains this reaction, exclude it from the available reactions list
             if (MainWindow.SOP.Protocol.scenario.environment.comp.reactions_dict.ContainsKey(cr.entity_guid) == true)
